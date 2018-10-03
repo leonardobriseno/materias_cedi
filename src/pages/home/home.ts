a5b671c13d56dd63eda0288d211f67f182b2570c
alumno Secundaria <al3201@cedi.local>
2018-10-03 08:07:57 -0500

    Dibujo ya esta

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,18 +1,24 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { HistoriaPage } from '../historia/historia';
-
+import { DibujoPage } from '../dibujo/dibujo';
+ 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
   historia = HistoriaPage;
+  dibujo = DibujoPage;
   constructor(public navCtrl: NavController) {
 
   }
   clickHistoria ()
   {
     this.navCtrl.push (this.historia);
+  }
+  clickDibujo ()
+  {
+    this.navCtrl.push (this.dibujo);
   }
 }
